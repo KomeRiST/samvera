b794bb5c40f1edc8060fc49c00853c639b2a8e82
Юрий Комерист <komerist1993-93@mail.ru>
2019-08-22 02:14:18 +0500

    +

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 ﻿from django.contrib import admin
 from django import forms
 from app.models import *
+from app.models import ColorField
 #from nested_inline.admin import NestedModelAdmin, NestedStackedInline, NestedTabularInline
 
 admin.site.register(StatusOrder)
@@ -8,9 +9,15 @@ admin.site.register(PotentialClient)
 admin.site.register(Orders)
 admin.site.register(OrderTovary)
 admin.site.register(Tovar)
-admin.site.register(Gallery)
 admin.site.register(OrderTovaryVariaciya)
 #admin.site.register(Variaciya)
+
+
+class GallAdmin(admin.ModelAdmin):
+    model = Gallery
+    list_display = ['image_img',]
+
+admin.site.register(Gallery, GallAdmin)
 
 class ImageInline(admin.TabularInline):
     model = Gallery
@@ -20,6 +27,16 @@ class ImageInline(admin.TabularInline):
 #@admin.register(Variaciya)
 class VariaciyaAdm(admin.ModelAdmin):
     inlines = [ImageInline,]
+    formfield_overrides = {
+        ColorField: {
+            'widget': forms.TextInput(
+                attrs={
+                    'type': 'color',
+                    'style': 'height: 25px; width: 100px'
+                    }
+                )
+            }
+        }
 
 admin.site.register(Variaciya, VariaciyaAdm)
 
